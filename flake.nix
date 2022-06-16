915515aa9e020359ba24e757ee0e995538f18406
Nicole Prindle <nprindle18@gmail.com>
2022-06-15 22:48:25 -0400

    Rework nix setup

--- flake.nix
+++ flake.nix
@@ -5,12 +5,42 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages.resume = import ./default.nix { inherit pkgs; };
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tex-env = pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+          scheme-small
+
+          titlesec
+          preprint
+          marvosym
+          enumitem
+          sourcesanspro
+          ly1
+          cm-super
+
+          latexmk;
+      };
+    in
+    {
+      packages.resume = pkgs.stdenv.mkDerivation {
+        name = "resume";
+        src = ./.;
+
+        nativeBuildInputs = [ pkgs.gnumake ];
+        buildInputs = [ tex-env ];
+
+        buildPhase = "make";
+        installPhase = ''
+          mkdir -p "$out"
+          make install
+        '';
+      };
       defaultPackage = self.packages.${system}.resume;
 
-      devShell = import ./shell.nix { inherit pkgs; };
+      devShell = pkgs.mkShell {
+        buildInputs = [ tex-env ];
+      };
     }
   );
 }
--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {}
-}:
-
-let tex-env = import ./tex.nix { inherit pkgs; };
-in pkgs.mkShell {
-  buildInputs = [ tex-env ];
-}
